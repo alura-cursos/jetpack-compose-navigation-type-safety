b1cddfd70eae54694f59a5cdda51f33dbd3d44cb
Alex Felipe <alexfelipevieira@gmail.com>
2023-01-16 16:04:57 -0300

    3.2

--- app/src/main/java/br/com/alura/panucci/navigation/DrinksNavigation.kt
+++ app/src/main/java/br/com/alura/panucci/navigation/DrinksNavigation.kt
@@ -7,16 +7,23 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import br.com.alura.panucci.model.Product
 import br.com.alura.panucci.ui.screens.DrinksListScreen
 import br.com.alura.panucci.ui.viewmodels.DrinksListViewModel
 
 internal const val drinksRoute = "drinks"
+const val uri = "alura://panucci.com.br"
 
 fun NavGraphBuilder.drinksScreen(
     onNavigateToProductDetails: (Product) -> Unit
 ) {
-    composable(drinksRoute) {
+    composable(
+        drinksRoute,
+        deepLinks = listOf(navDeepLink {
+            uriPattern = "$uri/$drinksRoute"
+        })
+    ) {
         val viewModel = viewModel<DrinksListViewModel>()
         val uiState by viewModel.uiState.collectAsState()
         DrinksListScreen(
